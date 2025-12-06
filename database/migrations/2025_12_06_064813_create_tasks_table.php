302530e76c4ab934cab64bf8fa3a507c14885eb8
tecsxpert <dev@tecsxpert.xyz>
2025-12-06 12:22:12 +0530

    added database migration

---
+++ database/migrations/2025_12_06_064813_create_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status',['todo','in_progess','done'])->default('todo');
+            $table->enum('priority',['low','medium','high'])->default('medium');
+            $table->date('due_date')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('status');
+            $table->index('pirority');
+            $table->index('due_date');
+            $table->index('create_at');
+            $table->index(['status','priroity']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
